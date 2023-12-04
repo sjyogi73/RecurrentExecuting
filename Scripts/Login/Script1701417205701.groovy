346301d655a5c64647c6159ba0153dba2de25788
sjyogi73 <sjyogi73@gmail.com>
2023-12-04 10:51:57 +0530

    Added changes in Login

--- Scripts/Login/Script1701417205701.groovy
+++ Scripts/Login/Script1701417205701.groovy
@@ -5,10 +5,9 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable
 
 def dayOfWeek = new Date().format("EEEE")
-//println "Today is: $dayOfWeek"
 
-if(!dayOfWeek=='Saturday' || !dayOfWeek=='Sunday')
-{
+if (!(dayOfWeek in ["Saturday", "Sunday"])) {
+	
 	WebUI.openBrowser(GlobalVariable.url)
 	
 	WebUI.setText(findTestObject('Object Repository/Page_greytHR IDP/input_username'), GlobalVariable.user)
@@ -18,4 +17,7 @@ if(!dayOfWeek=='Saturday' || !dayOfWeek=='Sunday')
 	WebUI.click(findTestObject('Page_greytHR IDP/button_Log in'))
 	
 	WebUI.click(findTestObject('Page_greytHR IDP/button_SignInOut'))
+
+} else {
+	println "Week off : $dayOfWeek"
 }
