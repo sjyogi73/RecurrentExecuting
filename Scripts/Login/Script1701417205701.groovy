53122adc3793095efffc8aed4f46814ac88c4776
sjyogi73 <sjyogi73@gmail.com>
2024-02-06 12:04:42 +0530

    Added Listener

--- Scripts/Login/Script1701417205701.groovy
+++ Scripts/Login/Script1701417205701.groovy
@@ -4,20 +4,13 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 import internal.GlobalVariable
 
-def dayOfWeek = new Date().format("EEEE")
 
-if (!(dayOfWeek in ["Saturday", "Sunday"])) {
-	
-	WebUI.openBrowser(GlobalVariable.url)
-	
-	WebUI.setText(findTestObject('Object Repository/Page_greytHR IDP/input_username'), GlobalVariable.user)
-	
-	WebUI.setEncryptedText(findTestObject('Object Repository/Page_greytHR IDP/input_password'), GlobalVariable.password)
-	
-	WebUI.click(findTestObject('Page_greytHR IDP/button_Log in'))
-	
-	WebUI.click(findTestObject('Page_greytHR IDP/button_SignInOut'))
+WebUI.openBrowser(GlobalVariable.url)
 
-} else {
-	println "Week off : $dayOfWeek"
-}
+WebUI.setText(findTestObject('Object Repository/Page_greytHR IDP/input_username'), GlobalVariable.user)
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_greytHR IDP/input_password'), GlobalVariable.password)
+
+WebUI.click(findTestObject('Page_greytHR IDP/button_Log in'))
+
+WebUI.click(findTestObject('Page_greytHR IDP/button_SignInOut'))
