0f618b13af479ffcea7f245e8634fcccd3efd42a
sjyogi73 <sjyogi73@gmail.com>
2024-08-13 18:12:06 +0530

    first commit

--- Scripts/Login/Script1701417205701.groovy
+++ Scripts/Login/Script1701417205701.groovy
@@ -1,20 +1,30 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import org.openqa.selenium.Keys as Keys
 
-import internal.GlobalVariable
+WebUI.openBrowser('')
 
+WebUI.navigateToUrl('https://shloklabs-adroitpayroll.greythr.com/')
 
-if (!GlobalVariable.isSkip) {
-	
-	WebUI.openBrowser(GlobalVariable.url)
-	
-	WebUI.setText(findTestObject('Object Repository/Page_greytHR IDP/input_username'), GlobalVariable.user)
-	
-	WebUI.setEncryptedText(findTestObject('Object Repository/Page_greytHR IDP/input_password'), GlobalVariable.password)
-	
-	WebUI.click(findTestObject('Page_greytHR IDP/button_Log in'))
-	
-	WebUI.click(findTestObject('Page_greytHR IDP/button_SignInOut'))
+WebUI.click(findTestObject('Object Repository/Signin/Page_greytHR/button_Sign In-ydrated-af9'))
 
-	}
+WebUI.click(findTestObject('Object Repository/Signin/Page_greytHR/span_Client Location-ydrated-6f7'))
+
+WebUI.click(findTestObject('Object Repository/Signin/Page_greytHR/div_Client Location-ydrated-585'))
+
+WebUI.click(findTestObject('Object Repository/Signin/Page_greytHR/button_Sign In-ydrated-a44'))
+
